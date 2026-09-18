ae11edc4a21e83b9186ddae64dd7204e14daacc6
Faisal Abrar <erfaisalabrar@gmail.com>
2026-09-18 11:48:26 +0530

    first

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+const express = require('express')
+
+const app = express()
+const PORT = 8080
+
+app.get('/', (req, res) => {
+    return res.json({ msg: 'Hello from the server v2 deploy\n' })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running on PORT ${PORT}`)
+})
